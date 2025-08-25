a991f10d1040ce1c21739e40bbb87a8ed7a8065f
Minal-dev-hub <buttminahil205@gmail.com>
2025-08-25 14:25:53 +0500

    first commit

---
+++ src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import hero from "../../assets/hero.jpg"
+
+const HeroSection = () => {
+  return (
+    <div>
+      <img src={hero} alt="" />
+    </div>
+  )
+}
+
+export default HeroSection
---
+++ src/pages/order/Order.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useContext } from 'react';
+import myContext from '../../context/data/myContext';
+
+const Order = () => {
+    
+  return (
+    <div>
+      order
+    </div>
+  )
+}
+
+export default Order
